eb127d1a04a6b98411b09d87c0d739edf9f0840a
Niklas Marion <niklas.marion@gmx.de>
2021-01-14 09:34:18 +0000

    chore: add nuxt eslint

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,20 +1,20 @@
 export default {
-  target: "static",
+  target: 'static',
   telemetry: false,
   ssr: false,
   router: {
-    base: process.env.NODE_ENV === "github" ? "/www/" : "/",
-    extendRoutes(routes, resolve) {
+    base: process.env.NODE_ENV === 'github' ? '/www/' : '/',
+    extendRoutes (routes, resolve) {
       routes.push({
         name: 'Schülerlabore',
         path: '/Schülerlabore',
         component: resolve(__dirname, 'pages/Schülerlabore.vue')
       },
-        {
-          name: 'Förderverein',
-          path: '/Förderverein',
-          component: resolve(__dirname, 'pages/Förderverein.vue')
-        }
+      {
+        name: 'Förderverein',
+        path: '/Förderverein',
+        component: resolve(__dirname, 'pages/Förderverein.vue')
+      }
       )
     }
   },
@@ -29,93 +29,96 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { name: 'keywords', content: 'Schülerforschungszentrum, Schule, Forschen, MINT, Informatik, Physik, Workshops, Elearing' },
       {
-        hid: "og:title",
-        name: "og:title",
-        content: "Schülerforschungszentrum Saarlouis",
+        hid: 'og:title',
+        name: 'og:title',
+        content: 'Schülerforschungszentrum Saarlouis'
       },
       {
-        hid: "og:url",
-        name: "og:url",
-        content: "https://sfz-sls.de",
+        hid: 'og:url',
+        name: 'og:url',
+        content: 'https://sfz-sls.de'
       },
       {
-        hid: "og:type",
-        name: "og:type",
-        content: "website",
+        hid: 'og:type',
+        name: 'og:type',
+        content: 'website'
       },
       {
-        hid: "og:image",
-        name: "og:image",
-        content: "https://sfz-sls.de/background.jpg",
+        hid: 'og:image',
+        name: 'og:image',
+        content: 'https://sfz-sls.de/background.jpg'
       },
       {
-        hid: "og:description",
-        name: "og:description",
-        content: "",
+        hid: 'og:description',
+        name: 'og:description',
+        content: ''
       },
       {
-        hid: "twitter:title",
-        name: "twitter:title",
-        content: "Schülerforschungszentrum Saarlouis"
+        hid: 'twitter:title',
+        name: 'twitter:title',
+        content: 'Schülerforschungszentrum Saarlouis'
       },
       {
-        hid: "twitter:image",
-        name: "twitter:image",
-        content: "https://sfz-sls.de/background.jpg"
+        hid: 'twitter:image',
+        name: 'twitter:image',
+        content: 'https://sfz-sls.de/background.jpg'
       },
       {
-        hid: "twitter:description",
-        name: "twitter:description",
-        content: "",
+        hid: 'twitter:description',
+        name: 'twitter:description',
+        content: ''
       },
       {
-        hid: "twitter:url",
-        name: "twitter:url",
-        content: "https://sfz-sls.de",
+        hid: 'twitter:url',
+        name: 'twitter:url',
+        content: 'https://sfz-sls.de'
       },
       {
-        hid: "twitter:domain",
-        name: "twitter:domain",
-        content: "sfz-sls.de",
+        hid: 'twitter:domain',
+        name: 'twitter:domain',
+        content: 'sfz-sls.de'
       },
       {
-        hid: "twitter:card",
-        name: "twitter:card",
-        content: "summary_large_image",
+        hid: 'twitter:card',
+        name: 'twitter:card',
+        content: 'summary_large_image'
       },
       {
-        hid: "description",
-        name: "description",
-        content: "Das Schülerforschungszentrum bietet allen Schülerinnen und Schülern einen Ort, wo sie als ganze Klasse Workshops an selbst gewählten Themen aus dem MINT-Bereich unterstützt forschen können.",
+        hid: 'description',
+        name: 'description',
+        content: 'Das Schülerforschungszentrum bietet allen Schülerinnen und Schülern einen Ort, wo sie als ganze Klasse Workshops an selbst gewählten Themen aus dem MINT-Bereich unterstützt forschen können.'
       },
       {
-        hid: "robots",
-        name: "robots",
-        content: "index,follow",
-      },
+        hid: 'robots',
+        name: 'robots',
+        content: 'index,follow'
+      }
     ]
   },
   build: {
     parallel: true,
     terser: true,
-    sourceMap: true,
+    sourceMap: true
   },
+  buildModules: [
+    '@nuxtjs/eslint-module'
+  ],
   pwa: {
     meta: {
       title: 'Schülerforschungszentrum Saarlouis',
-      author: 'Schülerforschungszentrum Saarlouis',
+      author: 'Schülerforschungszentrum Saarlouis'
     },
     manifest: {
       name: 'Schülerforschungszentrum Saarlouis',
       short_name: 'SFZ',
       lang: 'de',
       display: 'browser'
-    },
+    }
   },
   plugins: ['~plugins/iframe'],
   modules: [['nuxt-buefy', { css: true, materialDesignIcons: false }], ['@nuxtjs/sitemap'], ['@nuxtjs/pwa']],
   sitemap: {
     hostname: 'https://sfz-sls.de/',
-    gzip: true,
-  },
-};
+    gzip: true
+  }
+}
